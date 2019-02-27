2c7d787ef2f5bc4e116fac022242a70f1d98e56c
Everson Antonio da Silva <eversonantoniodasilva@gmail.com>
2019-02-26 23:59:33 -0400

    Implementação final das endpoints com renderização via nunjucks.
    Signed-off-by: Everson Antonio da Silva <eversonantoniodasilva@gmail.com>

--- index.js
+++ index.js
@@ -1,18 +1,42 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
-nunjucks.configure("views", {
+const app = express()
+
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
-app.set("view engine", "njk");
-app.set(express.urlencoded({ extended: false }));
+const checaParametroIdadeMiddleware = (req, res, next) => {
+  const { age } = req.query
+  console.log(age)
+  return !age ? res.redirect('/') : next()
+}
 
-app.get("/", (req, res) => {
-  return res.send("Hello");
-});
+app.set('view engine', 'njk')
+app.use(express.urlencoded({ extended: false }))
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  return res.render('index')
+})
+
+app.post('/check', (req, res) => {
+  var age = req.body.age
+  return idade >= 18
+    ? res.redirect(`/major/?age=${age}`)
+    : res.redirect(`/minor/?age=${age}`)
+})
+
+app.get('/major', checaParametroIdadeMiddleware, (req, res) => {
+  const { age } = req.query
+  return res.render('major', { age })
+})
+
+app.get('/minor', checaParametroIdadeMiddleware, (req, res) => {
+  const { age } = req.query
+  return res.render('minor', { age })
+})
+
+app.listen(3000)
